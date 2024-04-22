441ddedd0459319041ce4d1a5e18d5e28b67268c
Amar Kumar <amar.codingenthusiast@gmail.com>
2024-04-22 10:15:58 +0530

    404 added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
     return (
-        <Router>
+        <Router basename="/todo-list">
             <Header title="My Todo List" searchBar={true} />
             <Routes>
                 <Route path="/" element={<Todos />} />
